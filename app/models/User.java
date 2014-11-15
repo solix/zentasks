04a26dea75b1a406656348af9b7045ef905d0ed7
solix <soheil.jahanshahi@gmail.com>
2014-11-15 02:57:56 +0100

    init

---
+++ app/models/User.java
@@ -0,0 +1,40 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.*;
+
+
+/**
+ * Created by 5olix on 11/14/2014.
+ */
+
+    @Entity
+    public class User extends Model{
+
+        @Id
+        public String email;
+        public String name;
+        public String password;
+
+
+    public User(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+    public static Finder<String,User> find=new Finder<String, User>(String.class,User.class);
+
+    public static User authenticate(String email, String password) {
+        return find.where().eq("email", email)
+                .eq("password", password).findUnique();
+    }
+
+
+
+
+
+
+
+}
+
